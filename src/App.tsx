386278054b0458a447db1e86677cf53b9a4aaf29
Jonlle <jonlle19@gmail.com>
2025-02-04 02:38:35 -0500

    refactor(app): integrate Footer and MainContainer
    Updated `App.tsx` to include `Footer` and `MainContainer`, ensuring a flexible layout with `flex flex-col min-h-screen`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { useTheme } from "./context/theme/useTheme";
+import { Header, MainContainer, Footer } from "./components/layout";
 
 function App() {
-  const { theme, toggleTheme } = useTheme();
   return (
-    <main className="h-screen w-full flex items-center justify-center p-4 text-primary">
-      <div className="text-center">
-        <h1 className="text-5xl md:text-6xl font-bold text-heading mb-4">
-          Portfolio
-          <span className="block w-20 h-1 bg-blue-500 mx-auto mt-4 rounded-full"></span>
-        </h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <MainContainer>
+        <h1 className="text-4xl font-bold text-heading">Mi Portfolio</h1>
         <p className="text-xl mt-6">Full Stack Developer | React Specialist</p>
-
-        <button
-          className="btn-primary mt-8"
-          type="button"
-          onClick={toggleTheme}
-        >
-          Change Theme
-          <span className="ml-2">{theme === "dark" ? "🌞" : "🌙"}</span>
-        </button>
-      </div>
-    </main>
+      </MainContainer>
+      <Footer />
+    </div>
   );
 }
 
